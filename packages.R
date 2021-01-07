538cff0172bf8967bbfae459c90348b1fe72a57d
jeksterslab <social.jeksterslab@gmail.com>
2021-01-08 02:15:04 +0800

    Added packages.R

---
+++ packages.R
@@ -0,0 +1,4 @@
+if (!require("remotes")) install.packages("remotes")
+if (!require("magick")) install.packages("magick")
+if (!require("bookdown")) install.packages("bookdown")
+if (!require("ram")) remotes::install_github("jeksterslab/ram")
